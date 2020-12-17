80162de41d4635b7e65ed8fa9fba4fcc5c5e800d
gustavo.palacios <gustavo.palacios@openpay.mx>
2020-12-16 23:29:26 -0600

    se agrega log

--- demoLibrary/src/main/java/com/example/demolibrary/MainLibrary.kt
+++ demoLibrary/src/main/java/com/example/demolibrary/MainLibrary.kt
@@ -1,11 +1,13 @@
 package com.example.demolibrary
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 
 class MainLibrary {
 
     fun printMessage(context: Context){
+        Log.i("Library", "Dentro de la libreria...")
         Toast.makeText(context, "Ya estamos dentro de la libreria", Toast.LENGTH_SHORT).show();
     }
 }
